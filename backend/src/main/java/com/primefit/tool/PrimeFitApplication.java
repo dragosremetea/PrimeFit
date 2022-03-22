456345c77ea1e2eb2fa44e754f1bae60993cf488
Roncea Marius-Alexandru <ronceamarius84@yahoo.com>
2022-03-22 23:15:43 +0200

    backend initialization
    I created the project using Spring

---
+++ backend/src/main/java/com/primefit/tool/PrimeFitApplication.java
@@ -0,0 +1,13 @@
+package com.primefit.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeFitApplication.class, args);
+	}
+
+}
