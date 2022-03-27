bd1007c0ff9b39182bff2a958879353310fcbd19
Roncea Marius-Alexandru <ronceamarius84@yahoo.com>
2022-03-27 18:19:17 +0300

    PFT-2 Create User Entity
    PFT-5 Create Service

---
+++ backend/src/main/java/com/primefit/tool/service/UserService.java
@@ -0,0 +1,35 @@
+package com.primefit.tool.service;
+
+import com.primefit.tool.model.User;
+import com.primefit.tool.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> listAll() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public void delete(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> get(Integer id) {
+        return userRepository.findById(id);
+    }
+}
